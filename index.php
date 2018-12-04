4c6709520e57b12dd31912bb758a8b2628ea1edc
Martinguicheteau <martin.guicheteau2@gmail.com>
2018-12-04 15:53:14 +0100

    creation d'un class person et d'une class dog

--- index.php
+++ index.php
@@ -2,24 +2,51 @@
 class Person{
 private $firstname;
 private $lastname;
+private $dog;
 
 public function __construct($pFirstname, $pLastname){
     $this->firstname = $pFirstname;
     $this->lastname = $pLastname;
 }
 
-public function speak(){
+public function getfirstname(){
+    return $this->firstname;
+}
 
+public function getlastname(){
+return $this->lastname;
+}
+
+public function speak(): string {
+    return "Je suis " . $this->firstname . " " . $this->lastname . " et mon chien est " . $this->dog->getName();
+}
+public function buy($pDog){
+    $this ->dog = $pDog;
+    $this->dog->setOwner($this);
 }
 }
 class Dog{
     private $name;
+    private $owner;
+
     public function __construct($pName){
         $this->name = $pName;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setOwner($pOwner){
+        $this->owner = $pOwner;
+    }
+    public function speak(): string {
+        return "Je suis ".$this->name." et mon maitre est ".$this->owner->getfirstname()." ".$this->owner->getlastname();
     }
 }
 
 $person = new Person ("John", "Doe");
-var_dump($person);
-$name = new Dog ("Jhonny");
-var_dump($name);
+$dog = new Dog ("Jhonny");
+$person->buy($dog);
+var_dump($person->speak());
+var_dump($dog->speak());
