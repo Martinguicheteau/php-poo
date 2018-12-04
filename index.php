078eaf861c800ffb293481f6618d6423169cc682
Martinguicheteau <martin.guicheteau2@gmail.com>
2018-12-04 11:45:56 +0100

    creation de class

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
-var_dump(expression 'coucou');
+class  Person { 
+    private $firstname;
+    private $lastname;
+
+    public function setFirstname($pFirstname){
+        $this->firstname = $pFirstname;
+    }
+    Public function setLastname($pLastname){
+        $this->lastname = $pLastname;
+    }
+}
+
+$person1 = new Person();
+//$Person1->firstname = "John";
+//$Person1->lastname = "Doe";
+$person1->setFirstname("John");
+$person1->setLastname("Doe");
+
+var_dump($person1);
+
+//$Person2 = new Person();
+//$Person2->firstname = "John";
+//$Person2->lastname = "Die";
+
+//var_dump($Person2);
