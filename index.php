73308bbe3714d4fd3f73bf06e9fbe59263f30e40
Martinguicheteau <martin.guicheteau2@gmail.com>
2018-12-04 12:13:14 +0100

    Ajouter la methode speak

--- index.php
+++ index.php
@@ -6,23 +6,40 @@ class  Person {
     public function setFirstname($pFirstname){
         $this->firstname = $pFirstname;
     }
+
+    public function getFirstname(){
+        return $this->firstname;
+    }
+
     Public function setLastname($pLastname){
         $this->lastname = $pLastname;
+    }
+
+    public function getLastname(){
+        return $this->lastname;
+    }
+    Public function speak(){
+        return "hello my name is " . $this->firstname . " " . $this->lastname;
     }
 }
 
 $person1 = new Person();
 //$Person1->firstname = "John";
 //$Person1->lastname = "Doe";
+
+
 $person1->setFirstname("John");
 $person1->setLastname("Doe");
 
-var_dump($person1);
+var_dump($person1->speak());
+//var_dump($person1);
+//var_dump($person1->getFirstname());
+//var_dump($person1->getLastname());
 
-$person2 = new Person();
+//$person2 = new Person();
 //$Person2->firstname = "John";
 //$Person2->lastname = "Die";
-$person2->setFirstname("John");
-$person2->setLastname("Die");
+//$person2->setFirstname("John");
+//$person2->setLastname("Die");
 
-var_dump($person2);
+//var_dump($person2);
